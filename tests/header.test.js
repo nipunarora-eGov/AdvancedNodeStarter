c96d2eaccad2ed7d2928bd538823a3618e090897
Nipun <aroranipun1@gmail.com>
2024-07-20 13:40:19 +0530

    updated tests

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,27 +1,34 @@
-const puppeteer = require('puppeteer');
-let browser, page;
+const Page = require('./helpers/page');
+
+let page;
 
 beforeEach(async () => {
-  browser = await puppeteer.launch({
-    headless: false,
-  });
-  page = await browser.newPage();
+  page = await Page.build();
   await page.goto('localhost:3000');
 });
 
 afterEach(async () => {
-  await browser.close();
+  await page.close();
 });
 
-test('we can launch a browser', async () => {
-  const text = await page.$eval('a.brand-logo', (el) => el.innerHTML);
+test('the header has the correct text', async () => {
+  const text = await page.getContentsOf('a.brand-logo');
 
   expect(text).toEqual('Blogster');
 });
 
 test('clicking login starts oauth flow', async () => {
   await page.click('.right a');
+
   const url = await page.url();
 
   expect(url).toMatch(/accounts\.google\.com/);
+});
+
+test('When signed in, shows logout button', async () => {
+  await page.login();
+
+  const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
+
+  expect(text).toEqual('Logout');
 });
