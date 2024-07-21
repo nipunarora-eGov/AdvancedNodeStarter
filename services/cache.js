050170bfb67eb74f07af4bdd43a55f9b6eb7b9cf
Nipun <aroranipun1@gmail.com>
2024-07-21 17:56:30 +0530

    updated configuration

--- services/cache.js
+++ services/cache.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 const redis = require('redis');
 const util = require('util');
-
+const keys = require('../config/keys');
 const redisUrl = 'redis://127.0.0.1:6379';
-const client = redis.createClient(redisUrl);
+const client = redis.createClient(keys.redisUrl);
 // Promisifying the client.hget and client.hset functions
 client.hget = util.promisify(client.hget);
 // client.hset = util.promisify(client.hset);
